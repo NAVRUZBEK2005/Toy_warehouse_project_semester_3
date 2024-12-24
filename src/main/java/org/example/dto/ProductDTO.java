d1e9084b0824def2647b398bf9d3c05556286f30
navruz_fayzullaev <Navruz_Fayzullaev@student.itpu.uz>
2024-12-24 22:15:32 +0500

    ONLY LEFT ADMIN PANEL AND USER PANEL

---
+++ src/main/java/org/example/dto/ProductDTO.java
@@ -0,0 +1,17 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDTO {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private Long price;
+    private Integer categoryId;
+}
---
+++ src/main/java/org/example/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package org.example.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.RoleEnum;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private RoleEnum role;
+    private String password;
+    private String phoneNumber;
+}
